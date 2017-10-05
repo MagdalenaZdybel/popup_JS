686fc62b3ac878e7f80e83ba6920b6d2154b46b6
Magda <magda@motta.com.pl>
2017-10-05 12:39:36 +0200

    old methods

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ prompt
 window.onload = function()
 {
 	
+        alert("Czeœæ Magda. Mi³ego dnia");
+        
+        var odpowiedz = confirm("Czy jesteœ pewna ¿e chcesz usun¹c konto?");
+        
+        if (odpowiedz)
+        {
+          
+        //
+        alert("konto zosta³o usuniête");
+        
+        };
+        
+        var result = prompt("wpisz swoje imiê: ");
+        
+        alert("Bye Bye " + result);
+        
+        
 };
